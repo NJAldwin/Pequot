f958b33ca9b570c67a7aa3980c699107f170e60d
NJAldwin <Nick@Aldwin.us>
2011-06-22 00:18:45 -0400

    Bump version to 0.4.3

--- Pequot/Properties/AssemblyInfo.cs
+++ Pequot/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.4.2.*")]
-[assembly: AssemblyFileVersion("0.4.2.0")]
+[assembly: AssemblyVersion("0.4.3.*")]
+[assembly: AssemblyFileVersion("0.4.3.0")]
